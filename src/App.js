8a252ca65d143cd143bba8d4a7ef8c875a3e3c76
明孝 李 <limx1@allinpay.com>
2022-07-12 09:59:10 +0800

    添加路由

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import './App.css';
+import {Route, BrowserRouter, Routes} from "react-router-dom";
+import "./App.less";
 import Login from "./pages/login";
-
+import Home from "./pages/home";
 function App() {
-  return (
-    <>
-      <Login />
-    </>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/home" element={<Home/>} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
