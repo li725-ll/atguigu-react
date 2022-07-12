d4076459c749da36f0790751b222044769df9eb1
LMX <3521750635@qq.com>
2022-07-12 22:01:05 +0800

    feat: 安装less

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,17 +1,12 @@
-import { Button } from 'antd';
 import React from 'react';
+import './login.less'
 
 const Login = ()=>{
-    return (
-    <>
-        <Button type="primary">Primary Button</Button>
-        <Button>Default Button</Button>
-        <Button type="dashed">Dashed Button</Button>
-        <br />
-        <Button type="text">Text Button</Button>
-        <Button type="link">Link Button</Button>
-    </>
-    );
+  return (
+    <div className="login">
+      13231131
+    </div>
+  );
 }
 
 export default Login;
