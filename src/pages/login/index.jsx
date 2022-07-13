805d595beab181b722ac90da2cc680e1a512bff4
明孝 李 <limx1@allinpay.com>
2022-07-13 10:11:36 +0800

    feat: 登陆页面搭建完成

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { Button, Form, Input } from 'antd';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import './login.less'
+import logo from '../../assets/images/logo.png'
 
 const Login = ()=>{
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+
   return (
     <div className="login">
-      13231131
+      <header>
+        <img src={logo} alt="logo"/>
+        <h1>React项目: 后台管理系统</h1>
+      </header>
+      <section>
+        <h2>用户登录</h2>
+        <Form
+          name="normal_login"
+          className="login-form"
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: '请输入你的用户名!',
+              },
+            ]}
+          >
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="用户名" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: '请输入你的密码!',
+              },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="密码"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              登 陆
+            </Button>
+          </Form.Item>
+        </Form>
+      </section>
     </div>
   );
 }
